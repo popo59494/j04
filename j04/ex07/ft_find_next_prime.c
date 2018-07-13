fdc8d4687cda28a8fa12f9098c5e8c1ca18ed2a2
Clément FERNANDEZ <clement.frndz@gmail.com>
2018-07-13 23:53:35 +0200

    Corrige ex07 (gère les négatifs)

--- j04/ex07/ft_find_next_prime.c
+++ j04/ex07/ft_find_next_prime.c
@@ -6,7 +6,7 @@
 /*   By: apogorze <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/10 17:34:51 by apogorze          #+#    #+#             */
-/*   Updated: 2018/07/13 13:03:33 by apogorze         ###   ########.fr       */
+/*   Updated: 2018/07/13 23:53:28 by clement          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	ft_is_prime(int nb)
 
 int	ft_find_next_prime(int nb)
 {
-	while (nb >= 0)
+	while (1)
 	{
 		if (ft_is_prime(nb) == 1)
 			return (nb);
